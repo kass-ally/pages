47fdadb20f58a64551c43b855b2a506397050c2e
assandrssandra <assandrssandra@gmail.com>
2020-07-09 22:56:04 +0100

    check

--- assets/js/header.js
+++ assets/js/header.js
@@ -33,7 +33,7 @@ window.addEventListener("scroll", function(){
 
 // When btn is clicked, crossed is formed by lines
 btn.addEventListener("click", function(){
-    console.log("Cclicked");
+    console.log("CCclicked");
     var fullNav = document.getElementById("fullNav");
     var body = document.body;
     var main = document.querySelectorAll("main");
