ebc426cee309cac0a526fbc0386118347b9d0bcf
assandrssandra <assandrssandra@gmail.com>
2020-07-11 23:54:49 +0100

    upa

--- assets/js/photographs.js
+++ assets/js/photographs.js
@@ -15,16 +15,21 @@ window.addEventListener("click", function(event){
         var enlargedPhoto = document.getElementById("enlargedPhoto");
         var img = document.createElement("img");
         img.src = imgurl;
-        body.classList.add("noOf");
-        enlargedPhoto.appendChild(img);
-        enlargedPhoto.childNodes[0].classList.add("enlargedImg")
-        enlargeCont.classList.remove("hidden");
         if(!enlargeCont.classList.contains("hidden")){
             if (!event.target.closest('.enlargedImg')){
                 enlargeCont.classList.add("hidden");
                 enlargedPhoto.appendChild(img);
                 body.classList.remove("noOf");
+                enlargeCont.classList.remove("largephfadeIn");
+                enlargeCont.classList.add("largephfadeOut");
             } 
+        }else{
+            body.classList.add("noOf");
+            enlargedPhoto.appendChild(img);
+            enlargedPhoto.childNodes[0].classList.add("enlargedImg")
+            enlargeCont.classList.remove("hidden");
+            enlargeCont.classList.add("largephfadeIn");
+            enlargeCont.classList.remove("largephfadeOut");
         }
     }
 })
