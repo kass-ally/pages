da4d91bdc1cc1884edd0d01b4ce84388dc9ddd04
assandrssandra <assandrssandra@gmail.com>
2020-07-12 00:11:20 +0100

    update

--- assets/js/photographs.js
+++ assets/js/photographs.js
@@ -8,29 +8,29 @@ window.addEventListener("load", function(){
 })
 
 window.addEventListener("click", function(event){
-    // if(event.target.classList.contains("layer")){
+    if(event.target.classList.contains("layer")){
         var body = document.body;
         var imgurl = event.target.parentElement.style.backgroundImage.slice(4, -1).replace(/"/g, "");
         var enlargeCont = document.getElementById("enlarge");
         var enlargedPhoto = document.getElementById("enlargedPhoto");
         var img = document.createElement("img");
         img.src = imgurl;
-        if(!enlargeCont.classList.contains("hidden")){
-            if (!event.target.closest('.enlargedImg')){
-                enlargeCont.classList.add("hidden");
-                enlargedPhoto.removeChild(img);
-                body.classList.remove("noOf");
-                enlargeCont.classList.remove("largephfadeIn");
-                enlargeCont.classList.add("largephfadeOut");
-                console.log("remove");
-            } 
+        body.classList.add("noOf");
+        enlargedPhoto.appendChild(img);
+        enlargedPhoto.childNodes[0].classList.add("enlargedImg")
+        enlargeCont.classList.remove("hidden");
+        enlargeCont.classList.add("largephfadeIn");
+        enlargeCont.classList.remove("largephfadeOut");
         }else{
-            body.classList.add("noOf");
-            enlargedPhoto.appendChild(img);
-            enlargedPhoto.childNodes[0].classList.add("enlargedImg")
-            enlargeCont.classList.remove("hidden");
-            enlargeCont.classList.add("largephfadeIn");
-            enlargeCont.classList.remove("largephfadeOut");
+            if(!enlargeCont.classList.contains("hidden")){
+                if (!event.target.closest('.enlargedImg')){
+                    enlargeCont.classList.add("hidden");
+                    enlargedPhoto.removeChild(img);
+                    body.classList.remove("noOf");
+                    enlargeCont.classList.remove("largephfadeIn");
+                    enlargeCont.classList.add("largephfadeOut");
+                    console.log("remove");
+                } 
+            }
         }
-    // }
 })
